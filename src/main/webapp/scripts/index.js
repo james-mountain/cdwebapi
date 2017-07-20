b33fae1245f645ec4d4621cdd915822a39671560
Alex Rimmer <alex.rimmer@qa.com>
2017-07-20 16:12:49 +0000

    Added JQuery, updated functions

---
+++ src/main/webapp/scripts/index.js
@@ -0,0 +1,23 @@
+function getAllCDs() {
+    document.getElementById("outputHolder").innerHTML = "getAllCDs()"
+}
+
+function getOneCD() {
+    document.getElementById("outputHolder").innerHTML = "getOneCD()"
+}
+
+function deleteOneCD() {
+    document.getElementById("outputHolder").innerHTML = "deleteOneCD()"
+}
+
+function deleteAllCDs() {
+    document.getElementById("outputHolder").innerHTML = "deleteAllCDs()"
+}
+
+function createCD() {
+    document.getElementById("outputHolder").innerHTML = "createCD()"
+}
+
+function updateCD() {
+    document.getElementById("outputHolder").innerHTML = "updateCD()"
+}
