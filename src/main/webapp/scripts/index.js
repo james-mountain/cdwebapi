096ce458e18e493e6adc87423055486c918897bd
Alex Rimmer <alex.rimmer@qa.com>
2017-07-20 17:07:32 +0000

    Added functionality to the get single cd and the create cd function.

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -1,6 +1,6 @@
 function getAllCDs() {
     $.ajax({
-        url: 'rest/cd/json',
+        url: "rest/cd/json",
         type: 'GET',
         success: function(result) {
             document.getElementById("outputHolder").innerHTML = JSON.stringify(result);
@@ -9,7 +9,13 @@ function getAllCDs() {
 }
 
 function getOneCD() {
-    document.getElementById("outputHolder").innerHTML = "getOneCD()";
+    $.ajax({
+        url: ("rest/cd/json/" + document.getElementById("idField").value),
+        type: 'GET',
+        success: function(result) {
+            document.getElementById("outputHolder").innerHTML = JSON.stringify(result);
+        }
+    });
 }
 
 function deleteOneCD() {
@@ -21,7 +27,21 @@ function deleteAllCDs() {
 }
 
 function createCD() {
-    document.getElementById("outputHolder").innerHTML = "createCD()";
+    var artist = document.getElementById("artistField").value;
+    var genre = document.getElementById("genreField").value;
+    var title = document.getElementById("titleField").value;
+    var newCD = {"artistname": artist, "genre": genre, "title": title}
+
+    $.ajax({
+        type: 'POST',
+        url: ("rest/cd/json/" + document.getElementById("idField").value),
+        data: JSON.stringify(newCD),
+        dataType: "json",
+        success: function(result) {
+            document.getElementById("outputHolder").innerHTML = JSON.stringify(result);
+        }
+    });
+    //document.getElementById("outputHolder").innerHTML = "createCD()";
 }
 
 function updateCD() {
